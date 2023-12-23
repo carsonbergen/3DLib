58f279041031c8df9a732a27dd5690133b8e4c3c
Carson Bergen <bergencarson@gmail.com>
2023-12-22 17:24:56 -0700

    Basic error checking, fixed styling

--- Debug/CommandHandler.cs
+++ Debug/CommandHandler.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Linq;
 
 public partial class CommandHandler : Node
 {
@@ -9,19 +10,39 @@ public partial class CommandHandler : Node
 	public delegate void CommandResponseEventHandler(string response);
 	private string response;
 
-	public string CurrentCommand(Array arguments) 
+	private string EncodeError(string str)
 	{
-		return "[color=red]Needs more arguments.[/color]";
+		return "[color=red]" + str + "[/color]";
 	}
 
-	private void OnCommandEnteredSignal(string command) 
+	private string CommandErrorChecking(int minArgs, int maxArgs, int args)
+	{
+		if (args > maxArgs)
+			return EncodeError("Too many arguments");
+		else if (args < minArgs)
+			return EncodeError("Needs more arguments");
+		else
+			return null;
+	}
+
+	public string CurrentCommand(Array arguments)
+	{
+		var err = CommandErrorChecking(1, 1, arguments.Length);
+		if (err != null)
+			return err;
+		
+		return EncodeError("Arguments not recognized");
+	}
+
+	private void OnCommandEnteredSignal(string command)
 	{
 		var commandArray = command.Split(" ");
-		GD.Print(commandArray);
+		var arguments = commandArray.Skip(1).ToArray();
+
 		switch (commandArray[0])
 		{
 			case "current":
-				response = CurrentCommand(commandArray);
+				response = CurrentCommand(arguments);
 				EmitSignal(SignalName.CommandResponse, response);
 				break;
 			default:
