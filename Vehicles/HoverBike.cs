f717791807c090ba64896b1fb83d9572833bf524
Carson Bergen <bergencarson@gmail.com>
2024-01-02 13:56:48 -0700

    Hoverbike controller done
    However, I want to use rigid body 3d instead

--- Vehicles/HoverBike.cs
+++ Vehicles/HoverBike.cs
@@ -10,51 +10,45 @@ namespace ThreeDLib
         public float resetSpeed = 10f;
 
         [Export]
-        public float baseSpeed = 50f;
+        public float baseSpeed = 400f;
         [Export]
-        public float boostSpeed = 100f;
+        public float boostSpeed = 800f;
         [Export]
         public float brakingSpeed = 200f;
+        [Export]
+        public float maxBaseSpeed = 10f;
+        [Export]
+        public float maxBoostSpeed = 20f;
 
         private float speed = 0f;
-        private float previousVelocity = 0f;
 
         public override void _PhysicsProcess(double delta)
         {
-            GD.Print("Engine force: ", EngineForce);
-            GD.Print("Braking force: ", Brake);
-            GD.Print("Steering: ", Steering);
-            GD.Print("Linear velocity: ", LinearVelocity);
-
+            // Handle steering
             float steeringInput = Input.GetAxis("vehicle_right", "vehicle_left");
-
-            if (Input.IsActionJustPressed("vehicle_boost"))
-            {
-                previousVelocity = LinearVelocity.X;
-                speed = boostSpeed;
-            }
-            else
-            {
-                Brake = brakingSpeed * (float) delta;
-                speed = baseSpeed;
-            }
-
             Steering = steeringInput * 0.4f;
+
+            // Adjust speed of vehicle
+            if (Input.IsActionPressed("vehicle_boost"))
+                speed = boostSpeed;
+            else
+                speed = baseSpeed;
+
+            // Handle force application
             float engineForceInput = Input.GetAxis("vehicle_backward", "vehicle_forward");
             EngineForce = engineForceInput * speed;
 
+            // If no input, begin braking
             if (engineForceInput == 0f)
-            {
-                Brake = brakingSpeed * (float) delta;
-            }
+                Brake = brakingSpeed * (float)delta;
 
+            // Reset model roll
             if (steeringInput == 0f)
-            {
                 model.RotationDegrees = model.RotationDegrees with
                 {
-                    X = Mathf.Lerp(model.RotationDegrees.X, 0f, (float) delta * resetSpeed)
+                    X = Mathf.Lerp(model.RotationDegrees.X, 0f, (float)delta * resetSpeed)
                 };
-            }
+            // Rotate model
             else
             {
                 model.RotationDegrees = model.RotationDegrees with
@@ -68,6 +62,29 @@ namespace ThreeDLib
                 model.RotationDegrees = model.RotationDegrees with
                 {
                     X = Mathf.Clamp(model.RotationDegrees.X, -15, 15)
+                };
+            }
+        }
+
+        public override void _IntegrateForces(PhysicsDirectBodyState3D state)
+        {
+            // Clamp velocity
+            // Clamp for base speed
+            if (speed == baseSpeed)
+            {
+                LinearVelocity = LinearVelocity with
+                {
+                    X = Mathf.Clamp(LinearVelocity.X, -maxBaseSpeed, maxBaseSpeed),
+                    Z = Mathf.Clamp(LinearVelocity.Z, -maxBaseSpeed, maxBaseSpeed)
+                };
+            }
+            // Clamp for boost speed
+            else
+            {
+                LinearVelocity = LinearVelocity with
+                {
+                    X = Mathf.Clamp(LinearVelocity.X, -maxBoostSpeed, maxBoostSpeed),
+                    Z = Mathf.Clamp(LinearVelocity.Z, -maxBoostSpeed, maxBoostSpeed)
                 };
             }
         }
